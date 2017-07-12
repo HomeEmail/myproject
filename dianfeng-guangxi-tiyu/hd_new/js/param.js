2e853a481326f34cff956cc3674a7e0b765b12dc
HomeEmail <591694604@qq.com>
2017-07-12 15:32:12 +0800

    添加静态图片
    Signed-off-by: HomeEmail <591694604@qq.com>

--- dianfeng-guangxi-tiyu/hd_new/js/param.js
+++ dianfeng-guangxi-tiyu/hd_new/js/param.js
@@ -1,7 +1,7 @@
 //JavaScript Document
 var hostUrl = "http://10.0.4.46";//"http://222.68.210.60";
 var serverUrl  = hostUrl +":8088/dfManage";//:81/shanghai/index/
-var imgBasePath = hostUrl +":8088/df";  //:81/shanghai/基于首页栏目的图片
+var imgBasePath = '';//hostUrl +":8088/df";  //:81/shanghai/基于首页栏目的图片
 var req = null; // ajax requset对象
 
 var loadingDiv = null;
