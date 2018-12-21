c2eb1310cffd2d134895b33205101ee9e5b80e39
HomeEmail <591694604@qq.com>
2018-12-21 16:04:08 +0800

    11123

--- three/threeDemo/simpleCar/demo/obj.js
+++ three/threeDemo/simpleCar/demo/obj.js
@@ -2,7 +2,6 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(90,window.innerWidth/window.innerHeight,0.1,1000);
 camera.position.z = 0;
 camera.position.x = 0;
-camera.position.y = 200;
 
 var webGLRenderer = new THREE.WebGLRenderer({
     antialias : true //抗锯齿
@@ -10,7 +9,7 @@ var webGLRenderer = new THREE.WebGLRenderer({
 
 webGLRenderer.setPixelRatio(window.devicePixelRatio);
 webGLRenderer.setSize(window.innerWidth, window.innerHeight);
-webGLRenderer.setClearColor(0xeeeeee, 1);
+webGLRenderer.setClearColor(0x333333, 1);
 
 var light = new THREE.AmbientLight( 0xccbbaa, 0.5 );
 scene.add( light );
@@ -21,14 +20,14 @@ function Wheel() {
     var mtlLoader = new THREE.MTLLoader();
     //var self = this;
 
-    //mtlLoader.setPath('../assets/');
-   // mtlLoader.load('car3.mtl', function(materials) {
+    mtlLoader.setPath('../obj/');
+    mtlLoader.load('123.mtl', function(materials) {
 
-        //materials.preload();
+        materials.preload();
         var objLoader = new THREE.OBJLoader();
-        //objLoader.setMaterials(materials);
-        objLoader.setPath('../dangao/');
-        objLoader.load('dangao.obj', function(object) {
+        objLoader.setMaterials(materials);
+        objLoader.setPath('../obj/');
+        objLoader.load('123.obj', function(object) {
 
             object.children.forEach(function(item) {
                 item.castShadow = true;
@@ -38,7 +37,7 @@ function Wheel() {
             // wrapper.position.set(0,-5,-20);
             // wrapper.add(object);
 
-            object.position.set(0, 0, -800);
+            object.position.set(0, 0, -1000);
 
             scene.add(object);
             oo=object;
@@ -48,7 +47,7 @@ function Wheel() {
         }, function() {
             console.log('error');
         });
-    //});
+    });
 
 }
 start();
